34f1970b198f0801b7ef2f24faffa869c1f6d39c
malcolm staso <mstaso1@gmail.com>
2020-04-20 18:48:15 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 #board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def valid_move? (board, index)
-  index = index.to_i-1
+  index = index.to_i - 1
   index.between?(0,8) && !position_taken?(board, index)
 end
 
